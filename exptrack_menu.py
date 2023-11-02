6514c2ce550fe6466ec26016e3c56ff173d0d160
nerraDeXe <143480322+nerraDeXe@users.noreply.github.com>
2023-11-02 18:25:07 +0800

    v0.2
    Changed UI layout

--- exptrack_menu.py
+++ exptrack_menu.py
@@ -1,68 +1,89 @@
 from customtkinter import *
-import sqlite3
-import datetime
-from tkcalendar import DateEntry
-import tkinter.messagebox as mb
 from tkinter import *
 from tkinter import ttk
+from tkcalendar import DateEntry
+import datetime
+import tkinter.messagebox as mb
+import sv_ttk
+import sqlite3
 
 # Connecting to the Database
-connector = sqlite3.connect("Expense Tracker CTk.db")
+connector = sqlite3.connect("Expense Tracker Expenses.db")
 cursor = connector.cursor()
 
 cursor.execute(
-    'CREATE TABLE IF NOT EXISTS ExpenseTracker '
+    'CREATE TABLE IF NOT EXISTS Expenses '
     '(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, '
     'Date DATETIME, '
     'Payee TEXT, '
     'Note TEXT, '
     'Amount FLOAT, '
     'ModeOfPayment TEXT, '
-    'Category TEXT)'
+    'Category VARCHAR(20),'
+    'CONSTRAINT unique_exp UNIQUE (Date, Note))'
 )
 connector.commit()
 
 money_app = CTk()
 money_app.resizable(False, False)
-money_app.geometry("900x700")
+money_app.geometry("1250x700")
 money_app.title("Expense Tracker")
 
 set_appearance_mode("light")
+sv_ttk.set_theme("light")
 
 # Functions
+# Menu Functions
+def light_dark_mode(): # Switch between light/dark mode
+    ld = lightdark.get()
 
-def exphover(a):
-    addexpbut.configure(fg_color="#6500C3")
-
-def expnothover(a):
-    addexpbut.configure(fg_color='#7d57f8')
-
-def inchover(a):
-    addincbut.configure(fg_color="#6500C3")
-
-def incnothover(a):
-    addincbut.configure(fg_color="#7d57f8")
+    if ld:
+        set_appearance_mode("dark")
+        sv_ttk.set_theme("dark")
+        mainframe1left.configure(fg_color="#952f00")
+    else:
+        set_appearance_mode("light")
+        sv_ttk.set_theme("light")
+        mainframe1left.configure(fg_color="#E64900")
 
 def addexp_frame():
     mainframe1.pack_forget()
+    mainframe1left.pack_forget()
     mainframe2.pack(fill='both', expand='true')
 
 def home_frame():
     mainframe2.pack_forget()
-    mainframe1.pack()
+    mainframe1.pack(fill='both', side='right', expand='true')
+    mainframe1left.pack(fill='both', side='left', expand='true')
+    list_all_expenses()
+    show_expsum()
 
 # Functions for 'Expenses' menu
-
 def list_all_expenses():
     global connector, table
 
     table.delete(*table.get_children())
 
-    all_data = connector.execute('SELECT * FROM ExpenseTracker')
+    all_data = connector.execute('SELECT * FROM Expenses')
     data = all_data.fetchall()
 
     for values in data:
         table.insert('', END, values=values)
+
+def view_expense_details():
+    global table
+    global date, payee, note, amnt, MoP, cate
+    if not table.selection():
+        mb.showerror('No expense selected', 'Please select an expense from the table to view its details')
+    current_selected_expense = table.item(table.focus())
+    values = current_selected_expense['values']
+    expenditure_date = datetime.date(int(values[1][:4]), int(values[1][5:7]), int(values[1][8:]))
+    date.set_date(expenditure_date)
+    payee.set(values[2])
+    note.set(values[3])
+    amnt.set(values[4])
+    MoP.set(values[5])
+    cate.set(values[6])
 
 def selected_expense_to_words():
     global table
@@ -92,29 +113,29 @@ def remove_expense():
                          f'Are you sure that you want to delete the record of {values_selected[2]}')
 
     if surety:
-        connector.execute('DELETE FROM ExpenseTracker WHERE ID=%d' % values_selected[0])
+        connector.execute('DELETE FROM Expenses WHERE ID=%d' % values_selected[0])
         connector.commit()
 
         list_all_expenses()
         mb.showinfo('Record deleted successfully!',
                     'The record you wanted to delete has been deleted successfully')
 
-def remove_all_expenses():
-    surety = mb.askyesno('Are you sure?',
-                         "Are you sure that you want to delete all the expense items from the database?",
-                         icon='warning')
-
-    if surety:
-        table.delete(*table.get_children())
-
-        connector.execute('DELETE FROM ExpenseTracker')
-        connector.commit()
-
-        clear_fields()
-        list_all_expenses()
-        mb.showinfo('All Expenses deleted', 'All the expenses were successfully deleted')
-    else:
-        mb.showinfo('Ok then', 'The task was aborted and no expense was deleted!')
+# def remove_all_expenses():
+#     surety = mb.askyesno('Are you sure?',
+#                          "Are you sure that you want to delete all the expense items from the database?",
+#                          icon='warning')
+#
+#     if surety:
+#         table.delete(*table.get_children())
+#
+#         connector.execute('DELETE FROM Expenses')
+#         connector.commit()
+#
+#         clear_fields()
+#         list_all_expenses()
+#         mb.showinfo('All Expenses deleted', 'All the expenses were successfully deleted')
+#     else:
+#         mb.showinfo('Ok then', 'The task was aborted and no expense was deleted!')
 
 def clear_fields():
     global note, payee, amnt, MoP, date, table, cate
@@ -136,76 +157,90 @@ def adding_expense():
     except:
         mb.showerror('Inappropriate Value', 'Please enter numbers in the expense entry.')
 
-    if not date.get() or not payee.get() or not note.get() or not amnt.get() or not MoP.get() or not cate.get():
+    try:
+        if not date.get() or not payee.get() or not note.get() or not amnt.get() or not MoP.get() or not cate.get():
             mb.showerror('Fields empty!',
                      "Please fill all the missing fields before pressing the add button!")
-    else:
-        connector.execute('INSERT INTO ExpenseTracker (Date, Payee, Note, Amount, ModeOfPayment, Category) '
-                          'VALUES (?,?,?,?,?,?)', (date.get_date(), payee.get(), note.get(),
+        else:
+            connector.execute('INSERT INTO Expenses (Date, Payee, Note, Amount, ModeOfPayment, Category) '
+                          'VALUES (?,?,?,ROUND(?,1),?,?)', (date.get_date(), payee.get(), note.get(),
                                                    amnt.get(), MoP.get(), cate.get()))
-        connector.commit()
-
-        clear_fields()
-        list_all_expenses()
-        mb.showinfo('Expense added',
-                    'The expense whose details you just entered has been added to the database')
+            connector.commit()
+            clear_fields()
+            list_all_expenses()
+            mb.showinfo('Expense Added',
+                    'The expense  has been added to the database.')
+    except sqlite3.IntegrityError:
+        mb.showerror("Error", "Expense already exists. Try Again.")
 
 
 def edit_expense():
     global table
 
-    # noinspection PyGlobalUndefined
     def edit_existing_expense():
         global date, amnt, note, payee, MoP, cate
         global connector, table
-
-        current_selected_expense = table.item(table.focus())
-        contents = current_selected_expense['values']
-
+        current_selected_expenditure = table.item(table.focus())
+        contents = current_selected_expenditure['values']
         connector.execute(
-            'UPDATE ExpenseTracker SET Date = ?, Payee = ?, Description = ?, Amount = ?, ModeOfPayment = ?, '
-            'Category = ？WHERE ID= ?',
+            'UPDATE Expenses SET Date = ?, Payee = ?, Note = ?, Amount = ROUND(?,1),'
+            ' ModeOfPayment = ?, Category = ? WHERE ID = ?',
             (date.get_date(), payee.get(), note.get(), amnt.get(), MoP.get(), cate.get(), contents[0]))
         connector.commit()
-
         clear_fields()
         list_all_expenses()
-
-        mb.showinfo('Data edited',
-                    'We have updated the data and stored in the database as you wanted')
-        edit_btn.destroy()
+        mb.showinfo('Data edited', 'We have updated the data and stored in the database as you wanted')
         return
 
     if not table.selection():
         mb.showerror('No expense selected!',
                      'You have not selected any expense in the table for us to edit; please do that!')
         return
+    view_expense_details()
+    saveedit_button = CTkButton(master=frame_5, text='Save Edit', command=edit_existing_expense,
+                            corner_radius=5, hover_color="#808080", font=('Microsoft YaHei UI Light', 13),
+                            fg_color="#000000", )
+    saveedit_button.place(relx=0.75, rely=0.30)
+    saveedit_button.destroy()
 
-    edit_btn = CTkButton(master=frame_5, text='Edit expense', width=30, command=edit_existing_expense)
-    edit_btn.place(x=10, y=395)
+# UI Display Function
 
+def show_expsum():
+    cursor.execute('SELECT Amount FROM Expenses')
+    result = cursor.fetchall()
 
-# Main Frame (Home)
-mainframe1 = CTkFrame(master=money_app, fg_color="#000000", height=700, width=900)
-mainframe1.pack(fill='both')
+    total = sum(float(row[0]) for row in result)
+    moneycounter.configure(text="RM {:.2f}".format(total))
+
+# Left mainframe (Home)
+mainframe1left = CTkFrame(master=money_app, height=700, width=350, corner_radius=0)
+mainframe1left.pack(fill='both', side='left', expand='true')
+mainframe1left.configure(fg_color="#E64900")
+
+lightdark = CTkSwitch(master=mainframe1left, text="Dark Mode", command=light_dark_mode,
+                      onvalue=1, offvalue=0)
+lightdark.place(relx=0.05, rely=0.93)
+
+# Right mainframe (Home)
+mainframe1 = CTkFrame(master=money_app, fg_color="#000000", height=700, width=900, corner_radius=0)
+mainframe1.pack(fill='both', side='right', expand='true')
 
 # Frame 1 (invisible)
 
 frame_1 = CTkFrame(master=mainframe1, fg_color="#000000", height=150)
 frame_1.pack(fill='x', padx=2, pady=2)
 
-frame_1_1 = CTkFrame(master=frame_1, fg_color="#FFFFFF", height=150)
+frame_1_1 = CTkFrame(master=frame_1, height=150)
 frame_1_1.pack(fill='x')
 
-welclabel = CTkLabel(master=frame_1_1, text='Welcome, User', font=('Microsoft YaHei UI Light', 15))
-welclabel.place(relx=0.025, rely=0.07)
+welclabel = CTkLabel(master=frame_1_1, text='Welcome, User', font=('Microsoft YaHei UI Light', 18))
+welclabel.place(anchor='center', relx=0.5, rely=0.2)
 
-Balancecounter = CTkLabel(master=frame_1_1, text='Expenses', font=('Microsoft YaHei UI Light', 20), corner_radius=20)
-Balancecounter.place(anchor='center', relx=0.5, rely=0.20)
+Balancecounter = CTkLabel(master=frame_1_1, text='Expenses', font=('Microsoft YaHei UI Light', 18))
+Balancecounter.place(relx=0.02, rely=0.33)
 
-moneycounter = CTkLabel(master=frame_1_1, text=f'RM XXXX.XX', text_color='#FFFFFF', fg_color='#000000', width=200,
-                        height=50, font=('Microsoft YaHei UI Light', 15), corner_radius=20)
-moneycounter.place(anchor='center', relx=0.5, rely=0.5)
+moneycounter = CTkLabel(master=frame_1_1, text="", font=('Microsoft YaHei UI Light', 50))
+moneycounter.place(relx=0.02, rely=0.5)
 
 # Frame 2 (invisible)
 
@@ -214,26 +249,26 @@ frame_2.pack(fill='x', padx=2, pady=2)
 
 # Left Frame in Frame 2
 
-frame_2_1 = CTkFrame(master=frame_2, fg_color="#FFFFFF", height=200, width=450, border_width=2)
-frame_2_1.pack(side='left')
+frame_2_1 = CTkFrame(master=frame_2, height=100, width=450)
+frame_2_1.pack(side='left', padx=1, pady=1)
 
-addexpbut = CTkButton(master=frame_2_1, text='Add Expense', height=50, font=('Microsoft YaHei UI Light', 15), command=addexp_frame,
-                    fg_color="#7d57f8")
+addexpbut = CTkButton(master=frame_2_1, text='Add Expense', height=50, font=('Microsoft YaHei UI Light', 15), hover_color="#808080",
+                      fg_color="#000000", command=addexp_frame)
 addexpbut.place(anchor='center', relx=0.3, rely=0.5)
 addincbut = CTkButton(master=frame_2_1, text='Add Income', height=50, font=('Microsoft YaHei UI Light', 15),
-                      fg_color="#7d57f8")
+                      hover_color="#808080", fg_color="#000000", command=show_expsum)
 addincbut.place(anchor='center', relx=0.7, rely=0.5)
 
 # Right Frame in Frame 2
 
-frame_2_2 = CTkFrame(master=frame_2, fg_color="#FFFFFF", height=200, width=450, border_width=2)
-frame_2_2.pack(side='right')
+frame_2_2 = CTkFrame(master=frame_2, height=100, width=450)
+frame_2_2.pack(side='right', padx=1, pady=1)
 
 Balancecounter = CTkLabel(master=frame_2_2, text='Set A Budget', font=('Microsoft YaHei UI Light', 20), corner_radius=20, )
 Balancecounter.place(relx=0.05, rely=0.07)
 
-setbudget = CTkEntry(master=frame_2_2, height=50, width=150, font=('Arial', 15), corner_radius=10)
-setbudget.place(relx=0.1, rely=0.3)
+setbudget = CTkEntry(master=frame_2_2, height=25, width=150, font=('Arial', 15), corner_radius=10)
+setbudget.place(relx=0.1, rely=0.5)
 
 budget_button = CTkButton(master=frame_2_2, text='Save', corner_radius=20, hover_color="#808080", fg_color="#000000")
 budget_button.place(relx=0.5, rely=0.4)
@@ -241,7 +276,7 @@ budget_button.place(relx=0.5, rely=0.4)
 # Frame 3_1
 
 frame_3_1 = CTkFrame(master=mainframe1, fg_color="#FFFFFF", width=450, height=650, border_width=2)
-frame_3_1.pack(fill='x')
+frame_3_1.pack(fill='x', padx=20, pady=20)
 
 # Treeview Frame (Homepage)
 table = ttk.Treeview(frame_3_1, selectmode=BROWSE,
@@ -274,12 +309,6 @@ table.column('#7', width=105, stretch=NO)  # Category column
 table.place(relx=0, y=0, relheight=1, relwidth=1)
 
 list_all_expenses()
-
-# Button Binds
-addexpbut.bind('<Enter>', exphover)
-addexpbut.bind('<Leave>', expnothover)
-addincbut.bind('<Enter>', inchover)
-addincbut.bind('<Leave>', incnothover)
 
 # Main Frame (Expenses)
 mainframe2 = CTkFrame(master=money_app, fg_color="#ebebeb", height=700, width=900)
@@ -323,7 +352,7 @@ save_button = CTkButton(master=frame_5, text='Save', command=adding_expense,
                         corner_radius=5, hover_color="#808080", font=('Microsoft YaHei UI Light', 13), fg_color="#000000", )
 save_button.place(relx=0.75, rely=0.30)
 
-homepage_button = CTkButton(master=frame_5, command=home_frame or list_all_expenses(), text='Back to Home Page',
+homepage_button = CTkButton(master=frame_5, command=home_frame, text='Back to Home Page',
                             corner_radius=5, hover_color="#808080", font=('Microsoft YaHei UI Light', 13), fg_color="#000000")
 homepage_button.place(relx=0.0, rely=0.0)
 
@@ -405,14 +434,15 @@ table.column('#7', width=105, stretch=NO)  # Category column
 table.place(relx=0, y=0, relheight=1, relwidth=1)
 
 list_all_expenses()
-
-# Button Binds
-addexpbut.bind('<Enter>', exphover)
-addexpbut.bind('<Leave>', expnothover)
-addincbut.bind('<Enter>', inchover)
-addincbut.bind('<Leave>', incnothover)
+show_expsum()
 
 # Let Home Menu appear first
 mainframe2.pack_forget()
-
 money_app.mainloop()
+
+
+
+connector.close()
+
+
+
